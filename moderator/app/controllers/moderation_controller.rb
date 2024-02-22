9c39182c4fe333ea50e29f4b78b4c1b8dcc4bb5e
Sofiane.T <sofianetlemsanipro@gmail.com>
2024-02-22 04:57:53 +0100

    3

--- moderator/app/controllers/moderation_controller.rb
+++ moderator/app/controllers/moderation_controller.rb
@@ -1,14 +1,14 @@
 class ModerationController < ApplicationController
-    include ModeratedModel
-    def index
-    end
-  
-    def predict
-      text = params[:text]
-      language = params[:language]
-  
-      result = ModeratedModel.new.attributes_to_moderate(text, language)
-
-      render json: { prediction: result }
-    end
+  def index
   end
+  
+  def predict
+    text = params[:text]
+    language = params[:language]
+  
+    moderated_model = ModeratedModel.new
+    result = moderated_model.moderating(text, language)
+  
+    render json: { prediction: result }
+  end
+end
--- moderator/app/models/moderated_model.rb
+++ moderator/app/models/moderated_model.rb
@@ -1,7 +1,20 @@
 class ModeratedModel < ApplicationRecord
-    include Moderable
-    def index
-      end
-    def attributes_to_moderate
-        [:content, :title]
+  include Moderable
+  
+  def attributes_to_moderate
+    [:content, :language]
+  end
+  
+  before_save :moderate_attributes
+  
+  private
+  
+  def moderate_attributes
+    attributes_to_moderate.each do |attribute|
+      content = send(attribute)
+      language = 'fr' 
+      is_accepted = moderating(content, language)
+      self.is_accepted = is_accepted
     end
+  end
+end
